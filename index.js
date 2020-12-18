b20e16d807fffbf1f4f0de8ad18f8fee33eda2fa
safeplace12345 <safeplace12345@gmail.com>
2020-12-18 14:43:38 +0200

    implimenting the multiplication function

--- index.js
+++ index.js
@@ -8,17 +8,39 @@ const clearBtn = document.getElementById('clearbtn');
 
 const displayValue = (e) => {
     // Add another function if the value has a digit after a non word char
-if(screen.value.match(/[+]\d$/)){
-            evaluation()
-    } 
     screen.value += e.value;
 }
 const clearScr = () => {
     screen.value = '';
 }
 const evaluation = () => {
+    let val = screen.value.replace(/\s/g, '');
+    let newArr = val.split('')
+    let digits = []
+   calc_Mult(newArr,digits)
+    // console.log(index)
+    console.log(digits)
+
     screen.value = eval(screen.value).toFixed(2)
-    
 }
 
 
+const calc_Mult = (arr,digArr) => {
+    let index = arr.indexOf('*')
+    let index0 = index - 1;
+    let index1 = index + 1
+        for (let i = 0; i < arr.length; i++) {
+            if (arr.includes('*')) {
+               return digArr.push(arr[index0], arr[index], arr[index1])
+                break;
+            }
+        }}
+        const calc_Div = (arr,digArr) => {
+
+        }
+        const calc_Sub = (arr,digArr) => {
+
+        }
+        const calc_Add = (arr,digArr) => {
+
+        }
